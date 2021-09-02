b432d7fa7f7e019cf5e4feaec2bb3de42d703c7d
pwaycuilis <85208803+pwaycuilis@users.noreply.github.com>
2021-09-01 21:08:30 -0500

    Update writeBack.py

--- writeBack.py
+++ writeBack.py
@@ -13,18 +13,11 @@ class WriteBack:
 
     def run(self):
 
-        print(f"(writeBack16)self.destReg : {self.destReg}")
-        #print(f"(WB17)self.src1Reg : {self.src1Reg}")
-        #print(f"(WB17)self.src2Reg : {self.src2Reg}")
-
-        print(f"(WB18)self.postALUBuff[1] : {self.postALUBuff[1]}")
-        #print(f"self.R[self.destReg[self.postALUBuff[1]]] : {self.R[self.destReg[self.postALUBuff[1]]]}")
-        print(f"(WB20)self.postALUBuff[0]: {self.postALUBuff[0]}")
+    
         if self.postMemBuff != [-1, -1]:
             self.R[self.destReg[self.postMemBuff[1]]] = self.postMemBuff[0]  #self.R[self.destReg[
             self.postMemBuff = [-1, -1]
-            print(f"(WB24)self.postMemBuff = {self.postMemBuff}")
-
+           
 
 
         #if postALUBuff not empty, write value to register and then clear buff
@@ -45,12 +38,7 @@ class WriteBack:
         #     self.postALUBuff[0] = -1
         #     self.postALUBuff[1] = -1
 
-        # print("in Writeback: R, postMemBuff, postALUBuff, destReg")
-        # print(self.R)
-        # print(self.postMemBuff)
-        # print(self.postALUBuff)
-        # print(self.destReg)
-
+  
         #orignally uncommented**************VVVV
         #return WriteBack(self.R, self.postMemBuff, self.postALUBuff, self.destReg)
         #**********************************^^^^^^
